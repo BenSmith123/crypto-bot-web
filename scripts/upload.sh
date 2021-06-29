3bdb2a45d20a0ea709524c8ad3e5ed6ad375974f
Ben Smith <bensmith@mediaworks.co.nz>
2021-06-29 15:48:24 +1200

    upload script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,7 +1,7 @@
 
 # deploys all files in the build folder to S3
 
-aws s3 sync build s3://benjaminrsmith.com/crypto-bot \
+aws s3 sync build s3://cryptobot.nz \
 --exclude '.git/*' \
 --exclude '*.sh' \
 --acl public-read \
