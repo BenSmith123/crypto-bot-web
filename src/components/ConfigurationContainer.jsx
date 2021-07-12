dc304ef3f509903f537d971eb4467ae207e261b7
BenSmith123 <benjiiman12@gmail.com>
2021-07-12 14:05:02 +1200

    account form editable fields set up

--- src/components/ConfigurationContainer.jsx
+++ src/components/ConfigurationContainer.jsx
@@ -23,15 +23,10 @@ function renderContent() {
 
       <ul className="cryptoItemsContainer">
         {Object.keys(config.records).map((record) => (
-          <Collapsible
-            trigger={<CryptoItem recordName={record} />}
-            transitionTime={200}
 
-          >
+          <CryptoListItem key={record} recordName={record} />
 
-            <div className="editCrypto">hello</div>
 
-          </Collapsible>
         ))}
       </ul>
 
@@ -40,18 +35,157 @@ function renderContent() {
 }
 
 
-function CryptoItem(props) {
+function CryptoListItem(props) {
 
   const { recordName } = props;
   const record = config.records[recordName];
 
+  const { isHolding, isPaused } = record;
+  const {
+    buyPercentage,
+    sellPercentage,
+    stopLossPercentage,
+    warningPercentage,
+  } = record.thresholds;
+
   return (
-    <li className={`cryptoListItem${record.isHolding ? '-holding' : ''}`}>
+    <Collapsible
+      trigger={<CryptoListItemHeader record={record} recordName={recordName} />}
+      transitionTime={160}
+      onClick={console.log(props)}
+    >
+
+      <div className="editCryptoContainer">
+
+        {/* STATIC DATA */}
+
+        <div className="cryptoItemDetails">
+          <div>Status: {isHolding ? 'Holding' : 'Waiting to buy'}</div>
+          {isHolding
+            ? <div>Last buy price: {record.lastBuyPrice}</div>
+            : <div>Last sell price: {record.lastBuyPrice}</div>}
+          <div>Last transaction: {record.orderDate}</div>
+        </div>
+
+        {/* INPUT FIELDS */}
+
+        <div className="editCryptoInputItem">
+          Sell percentage:
+          <input
+            defaultValue={buyPercentage}
+            type="number"
+            // placeholder="Sell percentage"
+          />
+        </div>
+        <p>The increase that has to be met before selling the crypto-currency back into USDT</p>
+
+        <div className="editCryptoInputItem">
+          Buy percentage:
+          <input
+            defaultValue={sellPercentage}
+            type="number"
+            // placeholder="Sell percentage"
+          />
+        </div>
+        <p>The decrease that has to be met before buying back into the crypto</p>
+
+
+        <div className="editCryptoInputItem">
+          Stop-loss percentage:
+          <input
+            defaultValue={stopLossPercentage}
+            type="number"
+            placeholder="optional"
+          />
+        </div>
+        <p>
+          Threshold to sell at a loss -
+          once met your buy/sell percentages will be adjusted to break-even
+        </p>
+
+
+        <div className="editCryptoInputItem">
+          Warning percentage:
+          <input
+            defaultValue={warningPercentage}
+            type="number"
+            placeholder="optional"
+          />
+        </div>
+        <p>The decrease in % relative to the last purchase price</p>
+
+        {/* BUTTONS */}
+
+        {isHolding ? (
+          <button
+            type="button"
+            className="button"
+            onClick={() => console.log('hello')}
+          >
+            Sell
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="button-red"
+            onClick={() => console.log('hello')}
+          >
+            Buy
+          </button>
+        )}
+
+        {isPaused ? (
+          <button
+            type="button"
+            className="button"
+            onClick={() => console.log('hello')}
+          >
+            Unpause
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="button-red"
+            onClick={() => console.log('hello')}
+          >
+            Pause
+          </button>
+        )}
+
+        <button
+          type="button"
+          className="button-red"
+          onClick={() => console.log('hello')}
+        >
+          Remove
+        </button>
+
+        <button
+          type="button"
+          className="button-blue"
+          onClick={() => console.log('hello')}
+        >
+          Save
+        </button>
+
+      </div>
+
+
+    </Collapsible>
+
+  );
+}
+
+
+// header of crypto (expand/collapsible)
+function CryptoListItemHeader(props) {
+
+  const { recordName, record } = props;
+
+  return (
+    <li className={`cryptoListItemHeader${record.isHolding ? '-holding' : ''}`}>
       <div>{recordName}</div>
       <div>${record.limitUSDT} USD</div>
-      {/* <div>{record.isHolding ? '[holding]' : '[waiting]'}</div>
-      <div>{record.orderDate}</div> */}
-
     </li>
   );
 }
