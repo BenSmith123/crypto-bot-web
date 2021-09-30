b89a94b243a74b7126e4ff95fec6f2fc40a60874
BenSmith123 <benjiiman12@gmail.com>
2021-09-30 21:09:42 +1300

    fix eslint errors

--- src/styles/components/_inline.js
+++ src/styles/components/_inline.js
@@ -2,7 +2,7 @@
  * Styles for components that have to be in-line
  */
 
-export const SnackbarStyles = {
+export const SnackbarStyles = { // eslint-disable-line import/prefer-default-export
   style: {
     // zIndex: '1300',
     backgroundColor: '#000000ad',
