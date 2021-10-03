205a6144365d9f4059a3b2a371a22278fd934223
BenSmith123 <benjiiman12@gmail.com>
2021-10-03 20:26:46 +1300

    refactor config container to class to handle state from API responses & updating

--- src/components/ConfigurationContainer.jsx
+++ src/components/ConfigurationContainer.jsx
@@ -1,9 +1,9 @@
 
 /* eslint-disable react/jsx-props-no-spreading */
 
-import React, { useState, useContext } from 'react';
+import React from 'react';
 
-import { useSnackbar } from 'react-simple-snackbar';
+import { withSnackbar } from 'react-simple-snackbar';
 
 import AppContext from './AppContext';
 import CryptoListItem from './CryptoListItem';
@@ -15,110 +15,134 @@ import { CONFIG_ACTIONS } from '../helpers/constants';
 import { SnackbarStyles } from '../styles/components/_inline';
 
 
-function useForceUpdate() {
-  const [value, setValue] = useState(0); // eslint-disable-line
-  return () => setValue((value) => value + 1); // eslint-disable-line
-}
+// eslint-disable-next-line react/prefer-stateless-function
+class ConfigurationContainer extends React.Component {
 
-
-export default function ConfigurationContainer(props) {
-
-  const { config } = props;
-  const { accessToken } = useContext(AppContext);
-  const [openSnackbar] = useSnackbar(SnackbarStyles);
-
-  const forceUpdate = useForceUpdate();
-
-  // function to update and publish the config for all action buttons
-  const updateConfig = async (action, recordName) => {
-
-    switch (action) { // eslint-disable-line default-case
-      case CONFIG_ACTIONS.SELL: {
-        config.records[recordName].forceSell = true;
-        break;
-      }
-      case CONFIG_ACTIONS.BUY: {
-        config.records[recordName].forceBuy = true;
-        break;
-      }
-      case CONFIG_ACTIONS.PAUSE: {
-        config.records[recordName].isPaused = true; // TODO - this isn't supported in the cryptobot!
-        break;
-      }
-      case CONFIG_ACTIONS.UNPAUSE: {
-        config.records[recordName].isPaused = false; // TODO - this too ^
-        break;
-      }
-      case CONFIG_ACTIONS.REMOVE: {
-        delete config.records[recordName];
-        break;
-      }
-    }
-
-    // POST to publish config, display pop-up
-
-    openSnackbar('Saving...');
-    const result = await updateUserConfiguration(accessToken, config);
-
-    if (result.error) {
-      openSnackbar(`Error: ${result.errMessage}`);
-      return;
-    }
-
-    openSnackbar('Saved!');
-
-    forceUpdate(); // force component to re-render
-  };
-
-
-  if (!config) {
-    return (<div>Loading...</div>);
+  constructor(props) {
+    super(props);
+    this.state = {
+      config: props.config,
+    };
   }
 
-  if (!config.id || !config.records) {
-    return (<div>Error</div>);
+  static getDerivedStateFromProps(props, state) {
+    // update component state when config hasn't been loaded yet (API response from parent)
+    if (state.config === null) {
+      state.config = props.config;
+    }
+    return state;
   }
 
-  return (
-    <>
-      <ul className="cryptoItemsContainer">
 
-        <div>Bot status: {config.isPaused ? <Label text="Paused" /> : <LabelGreen text="Active" />}</div>
+  render() {
 
-        <div>Monitored currencies: {Object.keys(config.records).length}</div>
-        <div>Estimate total funds: $... USD ($... NZD)</div>
+    const { accessToken } = this.context;
+    const { openSnackbar } = this.props;
 
-        {Object.keys(config.records).map((record) => (
-          <CryptoListItem
-            key={record}
-            recordName={record}
-            config={config}
-            updateConfig={updateConfig}
-          />
-        ))}
+    const { config } = this.state;
+    const updatedConfig = { ...config };
 
-        <div className="cryptoItemsButtons">
+    // function to update and publish the config for all action buttons
+    const updateConfig = async (action, recordName) => {
 
-          <button
-            type="button"
-            className="button-red"
-          >
-            Pause bot
-          </button>
+      switch (action) { // eslint-disable-line default-case
+        case CONFIG_ACTIONS.SELL: {
+          updatedConfig.records[recordName].forceSell = true;
+          break;
+        }
+        case CONFIG_ACTIONS.BUY: {
+          updatedConfig.records[recordName].forceBuy = true;
+          break;
+        }
+        case CONFIG_ACTIONS.PAUSE: {
+          updatedConfig.records[recordName].isPaused = true; // TODO - this isn't supported in the cryptobot!
+          break;
+        }
+        case CONFIG_ACTIONS.UNPAUSE: {
+          updatedConfig.records[recordName].isPaused = false; // TODO - this too ^
+          break;
+        }
+        case CONFIG_ACTIONS.REMOVE: {
+          const newRecords = { ...config.records };
+          delete newRecords[recordName];
+          updatedConfig.records = newRecords;
+          break;
+        }
+      }
 
-          <button
-            type="button"
-            className="button-blue"
-            onClick={() => {
-            // append(defaultCurrencyValues);
-            }}
-          >
-            Add crypto currency
-          </button>
+      // POST to publish config, display pop-up
 
-        </div>
+      openSnackbar('Saving...');
 
-      </ul>
-    </>
-  );
+      const result = await updateUserConfiguration(accessToken, updatedConfig);
+
+      if (result.error) {
+        console.log(result);
+        openSnackbar(`Error: ${result.errMessage}`);
+        return;
+      }
+
+      // API returns the updated config, save it to state
+
+      this.setState({ config: result });
+
+      openSnackbar('Saved!');
+    };
+
+
+    if (!config) {
+      return (<div>Loading...</div>);
+    }
+
+    if (!config.id || !config.records) {
+      return (<div>Error</div>);
+    }
+
+    return (
+      <>
+        <ul className="cryptoItemsContainer">
+
+          <div>Bot status: {config.isPaused ? <Label text="Paused" /> : <LabelGreen text="Active" />}</div>
+
+          <div>Monitored currencies: {Object.keys(config.records).length}</div>
+          <div>Estimate total funds: $... USD ($... NZD)</div>
+
+          {Object.keys(config.records).map((record) => (
+            <CryptoListItem
+              key={record}
+              recordName={record}
+              config={config}
+              updateConfig={updateConfig}
+            />
+          ))}
+
+          <div className="cryptoItemsButtons">
+
+            <button
+              type="button"
+              className="button-red"
+            >
+              Pause bot
+            </button>
+
+            <button
+              type="button"
+              className="button-blue"
+              onClick={() => {
+                // append(defaultCurrencyValues);
+              }}
+            >
+              Add crypto currency
+            </button>
+
+          </div>
+
+        </ul>
+      </>
+    );
+  }
 }
+
+ConfigurationContainer.contextType = AppContext;
+
+export default withSnackbar(ConfigurationContainer, SnackbarStyles);
