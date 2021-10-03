8877c6b3c5140000f8f48baeb257f7eb83dddf24
BenSmith123 <benjiiman12@gmail.com>
2021-10-04 11:21:06 +1300

    remove crypto-icon API and load dynamic icons from static svg

--- src/components/CryptoIcon.jsx
+++ src/components/CryptoIcon.jsx
@@ -1,20 +1,16 @@
 
 import React from 'react';
 
-import { CRYPTO_ICONS_URL } from '../environment';
-
 export default function CryptoIcon(props) {
   const { name } = props;
   return (
-    <div className="cryptoIcon">
-      <img
-        className="fadein-image"
-        onError={(event) => event.target.style.display = 'none'} // eslint-disable-line
-        src={`${CRYPTO_ICONS_URL}white/${name.toLowerCase()}/32`}
-        alt={`${name.toLowerCase()}-icon`}
-        width="32"
-        height="32"
-      />
-    </div>
+    <img
+      className="fadein-image"
+      onError={(event) => event.target.src = '/crypto-icons/_empty.svg'} // eslint-disable-line
+      src={`/crypto-icons/${name.toLowerCase()}.svg`}
+      alt={`${name.toLowerCase()}-icon`}
+      width="32"
+      height="32"
+    />
   );
 }
--- src/environment.js
+++ src/environment.js
@@ -3,5 +3,4 @@ module.exports = {
   // WEB_API_URL: 'http://localhost:8081/',
   // WEB_API_URL: 'https://csezryhvsa.execute-api.ap-southeast-2.amazonaws.com/prod/', // original discord API
   WEB_API_URL: 'https://ffplybtk37.execute-api.ap-southeast-2.amazonaws.com/prod/',
-  CRYPTO_ICONS_URL: 'https://cryptoicons.org/api/',
 };
