3548e65fc268dfb4eee2e59a92e876d3329308fe
BenSmith123 <benjiiman12@gmail.com>
2021-10-08 13:53:55 +1300

    remove unused legacy components

--- src/views/Account.jsx
+++ src/views/Account.jsx
@@ -1,41 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable */
 
 import React, { useState, useContext } from 'react';
-import { useForm, useFieldArray } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 import AppContext from '../components/AppContext';
-import { LabelBlue } from '../components/Label';
 import Loader from '../components/Loader';
 import ConfigurationContainer from '../components/ConfigurationContainer';
 
-
-const exampleCurrencyValues = {
-  currencyCode: 'BTC',
-  sellPercentage: 5,
-  buyPercentage: -3,
-  limitUSDT: 100,
-  stopLossPercentage: -20,
-  warningPercentage: -10,
-};
-
-
-const defaultCurrencyValues = {
-  currencyCode: '',
-  sellPercentage: '',
-  buyPercentage: '',
-  limitUSDT: '',
-  stopLossPercentage: '',
-  warningPercentage: '',
-};
-
-const availableCurrencies = [
-  'BTC',
-  'DOGE',
-  'ETH',
-  'SHIB',
-];
 
 const PAGES = {
   configuration: 'configuration',
@@ -50,28 +21,10 @@ export default function Account(props) {
 
   const { config } = props;
 
-  const { register, control, handleSubmit } = useForm({
-    defaultValues: {
-      test: [
-        { ...exampleCurrencyValues },
-      ],
-    },
-  });
-
-  const { fields, append, remove } = useFieldArray(
-    {
-      control,
-      name: 'test',
-    },
-  );
-
-  const onSubmit = (data) => console.log(data);
 
   return (
     <div className="accountPage">
       <h1>Account</h1>
-
-      
 
       {isSignedIn && config ? (
         <>
@@ -88,15 +41,11 @@ export default function Account(props) {
           {page === PAGES.configuration ? (
             <>
               <ConfigurationContainer config={config} />
-
-              <form onSubmit={handleSubmit(onSubmit)}>
-                {/* <CryptoForm /> */}
-              </form>
             </>
           ) : (
             <>
               <p>Page is currently under development :)</p>
-              <Loader/>
+              <Loader />
             </>
           )}
         </>
@@ -114,107 +63,12 @@ export default function Account(props) {
               */}
             </p>
           ) : (
-            <Loader/>
+            <Loader />
           )}
         </>
       )}
 
     </div>
   );
-
-
-  function CryptoForm() {
-    return (
-
-      <div>
-        {fields.map((item, i) => (
-          <div className="inputBlock" key={item.id}>
-
-            <select
-              {...register(`test.${i}.currencyCode`)}
-            >
-              {availableCurrencies.map((c) => <option key={c} value={c}>{c}</option>)}
-            </select>
-            <div className="inputDescription">Crypto-currency code</div>
-
-
-            <input
-              defaultValue={`${item.sellPercentage}`}
-              type="number"
-              placeholder="Sell percentage"
-              {...register(`test.${i}.sellPercentage`)}
-            />
-            <div className="inputDescription">
-              The increase that has to be met before selling the crypto-currency
-              back into USDT
-            </div>
-
-
-            <input
-              defaultValue={`${item.buyPercentage}`}
-              type="number"
-              placeholder="Buy percentage"
-              {...register(`test.${i}.buyPercentage`)}
-            />
-            <div className="inputDescription">
-              The decrease that has to be met before buying back into the crypto
-            </div>
-
-
-            <input
-              defaultValue={`${item.limitUSDT}`}
-              type="number"
-              placeholder="Limit USDT"
-              {...register(`test.${i}.limitUSDT`)}
-            />
-            <div className="inputDescription">
-              <LabelBlue text="optional" />
-              The amount of USDT to use when trading the given crypto.
-              The limit will be adjusted on every sell transaction to continue trading with
-              additional gains/losses.
-              <br />
-              WARNING: If no amount is specified, all available USDT will be used.
-            </div>
-
-
-            <input
-              defaultValue={`${item.stopLossPercentage}`}
-              type="number"
-              placeholder="Stop loss percentage"
-              {...register(`test.${i}.stopLossPercentage`)}
-            />
-            <div className="inputDescription">
-              <LabelBlue text="optional" />
-              Threshold to sell at a loss. Once met your buy/sell percentages
-              will be adjusted to break-even
-            </div>
-
-
-            <input
-              defaultValue={`${item.warningPercentage}`}
-              type="number"
-              placeholder="Warning percentage"
-              {...register(`test.${i}.warningPercentage`)}
-            />
-            <div className="inputDescription">
-              <LabelBlue text="optional" />
-              The increase that has to be met before selling the crypto-currency
-              back into USDT
-            </div>
-
-            {/* <Controller
-              render={({ field }) => <input {...field} />}
-              name={`test.${index}.lastName`}
-              control={control}
-              defaultValue={item.lastName}
-            /> */}
-
-          </div>
-        ))}
-      </div>
-
-
-    );
-  }
 
 }
